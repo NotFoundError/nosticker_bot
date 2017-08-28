d002d4cc4a2291eaaa2cc05cd71ea7c16090a868
Gregory Petukhov <lorien@lorien.name>
2017-08-28 19:01:25 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+## No Sticker Bot
+
+This simple telegram bot was created to solve only one task - to delete
+FUCKINGLY annoying stickers. Since you add bot to the group and allow it to
+delete sticker messages it watches all new messages and delete stickers
+immediately.
+
+
+## Usage
+
+1. Add [@nosticker_bot](https://t.me/nosticker_bot) to your group.
+2. Go to group settings / users list / promote user to admin
+3. Enable only one item: Delete messages
+4. Click SAVE button
+5. Enjoy!
+
+
+## Source Code
+
+Check it here https://github.com/lorien/nosticker_bot
+
+
+## Questions, ideas, bugs
+
+Use [github issues](https://github.com/lorien/nosticker_bot/issues) or send message to telegram: [@madspectator](https://t.me/madspectator)
---
+++ nosticker_bot.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import json
+import logging
+import telebot
+
+
+def create_bot(api_token):
+    bot = telebot.TeleBot(api_token)
+
+    @bot.message_handler(content_types=['sticker'])
+    def handle_sticker(msg):
+        self.delete_message(msg.chat.id, msg.message_id)
+
+    @bot.message_handler(commands=['start', 'help'])
+    def handle_start_help(msg):
+        with open('README.rst') as inp:
+            data = inp.read()
+        bot.reply_to(msg, data)
+
+    return bot
+
+
+def main():
+    logging.basicConfig(level=logging.DEBUG)
+    with open('var/config.json') as inp:
+        config = json.load(inp)
+    bot = create_bot(config['api_token'])
+    bot.polling()
+
+
+if __name__ == '__main__':
+    main()
